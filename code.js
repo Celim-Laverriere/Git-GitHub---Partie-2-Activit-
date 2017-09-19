a064eee108533541de85eeb0d451d117679040f2
Celim Laverriere <laverriere.celim@gmail.com>
2017-09-19 11:45:26 +0200

    Insertion du code dans code.js pour interagir avec l'utilisateur

--- code.js
+++ code.js
@@ -1,3 +1,7 @@
 // Pour se présenter à son ordinateur
 
 var nomPrenom, age, sex;
+
+nomPrenom = (prompt("Bienvenue ! \n Merci, de saisir votre nom et votre prénom : "));
+age = Number(prompt("Précisez votre âge : "));
+sex = (prompt("Etez-vous une fille ou un garçon ? "));
